347fa89dd45bfdff46dc6c0f81900314e9eb5cb0
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-21 10:34:22 -0800

    Complete business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,29 @@
 // Business Logic
-function robogerReturn(num) {
+function beepBoop(num) {
   let numberArray = Array.from(num.toString()).map(Number);
   let robotArray = [];
-  numberArray.forEach(function (num) {
-    if (num === 1) {
-      robotArray.push("Beep!");
+  if (numberArray.includes(3)) {
+    return "Won't you be my neighbor?";
+  } else if (numberArray.includes(2)) {
+    return "Boop";
+  } else if (numberArray.includes(1)) {
+    return "Beep!";
+  } else if (!numberArray.includes(1, 2, 3))
+    for (let i = 0; i < num + 1; i++) {
+      if (i === 1) {
+        robotArray.push("Beep!");
+      } else if (i === 2) {
+        robotArray.push("Boop!");
+      } else if (i === 3) {
+        robotArray.push("Won't you be my neighbor?");
+      } else {
+        robotArray.push(i);
+      }
     }
-    console.log(robotArray);
-  });
+  return robotArray;
 }
 
-console.log(robogerReturn(123456));
+console.log(beepBoop(321));
 
 // UI Logic
 
@@ -19,3 +32,10 @@ console.log(robogerReturn(123456));
 //   } else {
 //     return num;
 //   }
+
+// numberArray.forEach(function (num) {
+// if (numberArray.includes(1)) {
+//     robotArray.splice("Beep!");
+//   }
+//   console.log(robotArray);
+// });
