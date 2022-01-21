867a41d66a44fc265827d337b97bf9868238fa69
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-21 15:03:43 -0800

    Add styling and edit UI

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,20 @@
 // Business Logic
 function beepBoop(num) {
   let numberArray = Array.from(num.toString()).map(Number);
-  console.log(numberArray);
-  console.log(typeof numberArray[0]);
   let robotArray = [];
   for (let i = 0; i <= num; i++) {
-    if (numberArray[i] === 3) {
-      robotArray.push("Won't you be my neighbor?, ");
-    } else if (numberArray.includes(2)) {
-      robotArray.push("Boop!, ");
-    } else if (numberArray.includes(1)) {
-      robotArray.push("Beep!, ");
+    const index = i.toString();
+    if (index.includes(3)) {
+      robotArray.push(`"Won't you be my neighbor?"`);
+    } else if (index.includes(2)) {
+      robotArray.push('"Boop!"');
+    } else if (index.includes(1)) {
+      robotArray.push('"Beep!"');
     } else if (i < num) {
-      robotArray.push(i + ", ");
-    } else {
       robotArray.push(i);
-    }
+    } else robotArray.push(i);
   }
-  return robotArray;
+  return robotArray.join(", ");
 }
 
 // UI Logic
@@ -30,28 +27,3 @@ $(document).ready(function () {
     $("#beep-boop-message").html(beepBoopMessage);
   });
 });
-
-// let numberArray = Array.from(num.toString()).map(Number);
-// let robotArray = [];
-// if (numberArray.includes(3)) {
-//   return "Won't you be my neighbor?";
-// } else if (numberArray.includes(2)) {
-//   return "Boop!";
-// } else if (numberArray.includes(1)) {
-//   return "Beep!";
-// } else if (!numberArray.includes(1, 2, 3))
-//   for (let i = 0; i <= num; i++) {
-//     if (i === 1) {
-//       robotArray.push("Beep!, ");
-//     } else if (i === 2) {
-//       robotArray.push("Boop!, ");
-//     } else if (i === 3) {
-//       robotArray.push("Won't you be my neighbor?, ");
-//     } else if (i < num) {
-//       robotArray.push(i + ", ");
-//     } else {
-//       robotArray.push(i);
-//     }
-//   }
-// return robotArray;
-// }
