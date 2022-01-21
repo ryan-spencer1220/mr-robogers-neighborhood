63c9b58359f76abe923107266ebca40350129b97
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-01-21 14:01:22 -0800

    WIP: Fix business logic to better reflect guidelines in prompt

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,24 @@
 // Business Logic
 function beepBoop(num) {
   let numberArray = Array.from(num.toString()).map(Number);
+  console.log(numberArray);
+  console.log(typeof numberArray[0]);
   let robotArray = [];
-  if (numberArray.includes(3)) {
-    return "Won't you be my neighbor?";
-  } else if (numberArray.includes(2)) {
-    return "Boop!";
-  } else if (numberArray.includes(1)) {
-    return "Beep!";
-  } else if (!numberArray.includes(1, 2, 3))
-    for (let i = 0; i <= num; i++) {
-      if (i === 1) {
-        robotArray.push("Beep!, ");
-      } else if (i === 2) {
-        robotArray.push("Boop!, ");
-      } else if (i === 3) {
-        robotArray.push("Won't you be my neighbor?, ");
-      } else if (i < num) {
-        robotArray.push(i + ", ");
-      } else {
-        robotArray.push(i);
-      }
+  for (let i = 0; i <= num; i++) {
+    if (numberArray[i] === 3) {
+      robotArray.push("Won't you be my neighbor?, ");
+    } else if (numberArray.includes(2)) {
+      robotArray.push("Boop!, ");
+    } else if (numberArray.includes(1)) {
+      robotArray.push("Beep!, ");
+    } else if (i < num) {
+      robotArray.push(i + ", ");
+    } else {
+      robotArray.push(i);
     }
+  }
   return robotArray;
 }
-
-console.log(beepBoop(5));
 
 // UI Logic
 $(document).ready(function () {
@@ -37,3 +30,28 @@ $(document).ready(function () {
     $("#beep-boop-message").html(beepBoopMessage);
   });
 });
+
+// let numberArray = Array.from(num.toString()).map(Number);
+// let robotArray = [];
+// if (numberArray.includes(3)) {
+//   return "Won't you be my neighbor?";
+// } else if (numberArray.includes(2)) {
+//   return "Boop!";
+// } else if (numberArray.includes(1)) {
+//   return "Beep!";
+// } else if (!numberArray.includes(1, 2, 3))
+//   for (let i = 0; i <= num; i++) {
+//     if (i === 1) {
+//       robotArray.push("Beep!, ");
+//     } else if (i === 2) {
+//       robotArray.push("Boop!, ");
+//     } else if (i === 3) {
+//       robotArray.push("Won't you be my neighbor?, ");
+//     } else if (i < num) {
+//       robotArray.push(i + ", ");
+//     } else {
+//       robotArray.push(i);
+//     }
+//   }
+// return robotArray;
+// }
